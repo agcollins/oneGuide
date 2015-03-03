c567778a9fb5b982c45ecbb693556b9161c8b222
Rachel <rachel.nicole.tucker@gmail.com>
2015-03-03 12:19:07 -0600

    made free champ rotation carousel and changed color scheme

--- basic.js
+++ basic.js
@@ -1,20 +1,44 @@
+//api key: 43e187ef-e56e-4f24-bd58-1dbdc841abff
 angular.module("oneGuideApp", ["ui.bootstrap"]);
-angular.module("oneGuideApp").controller("oneGuideController", function($scope){
+angular.module("oneGuideApp").controller("oneGuideController", function($scope, $http){
     $scope.selectedChamp = undefined;
+    $scope.champions = [];
+    $scope.champNames = [];
 
-    jQuery.getJSON('lolChamps.json', function(champs){
-	$scope.champions = champs;
-	$scope.champNames = [];
-	champs.forEach(function(champ){
-	    $scope.champNames.push(champ['name']);
-	});
+    $http.get("https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff").success(function(data) {
+	var champs = data["data"]
+	for (var key in champs){
+	    $scope.champions.push(champs[key]);
+	    $scope.champNames.push(champs[key]["name"]);
+	}
     });
 
+    $http.get("https://na.api.pvp.net/api/lol/na/v1.2/champion?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff&freeToPlay=true").success(function(data) {
+	$scope.freeChamps = [];
+	var freechampions = data["champions"];
+	freechampions.forEach(function(champ){
+	    $http.get("https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + champ["id"] + "?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff").success(function(data){
+		var name = data.name;
+		if(name === "Kha'Zix")
+		    name = "Khazix";
+		if(name === "Xin Zhao")
+		    name = "XinZhao";
+		data["imgsrc"] = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + name + "_0.jpg";
+		$scope.freeChamps.push(data);
+	    });
+	});
+    });
 });
 
 $(document).ready(function(){
     $(".my-players > li").bind('click', function(e){
-	var players = $(".my-players > li").removeClass("active");
-	$(this).addClass("active");
+	if($(this).hasClass('all-players')){
+	    $(".my-players > li").removeClass("active");
+	    $(this).addClass("active");
+	}
+	else{
+	    $(".all-players").removeClass("active");
+	    $(this).addClass("active");
+	}
     });
 });
