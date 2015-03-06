d51be3d6ba4ba82fb2bbb2e9e5f2e6f86d3cbf15
agcollins <agcollins@utexas.edu>
2015-03-05 23:59:40 -0600

    added angular file with module and controller for sidebar in champion.html

---
+++ ngchamp.js
@@ -0,0 +1,45 @@
+var app = angular.module('ChampionApp', ['ui.bootstrap']);
+
+app.controller('PanelController', function(){
+	this.allSelected = false;
+	this.players = [
+		'TheOddOne','Imaqtpie','Bjergsen' //we need to read this from a json file
+	];	
+	
+	this.selected = [];
+	this.select = function(name){
+		if(!name || (name === 'all' && !this.allSelected)){
+			this.selected=[]; //this is to prevent cases where a player is selected then added into the list again when the 'all' button is pushed. I don't know if this would cause any bugs, but I'm just doing this to be safe.
+			for(p in this.players){
+				this.selected.push(this.players[p]); //select all players if no parameter is passed or if 'all' is clicked
+			}
+
+			this.allSelected = true;
+		}
+
+		else if(name === 'all' && this.allSelected){
+			this.selected = [];
+			this.allSelected = false;
+		}
+
+		else { //name isn't 'all'
+			this.allSelected = false;	
+			if(this.selected.indexOf(name)>-1){
+				this.selected.splice(this.selected.indexOf(name), 1); //remove at index of name
+			}
+			else this.selected.push(name);
+
+			console.log(this.selected);
+		}
+	};
+	this.isSelected = function(name){
+		if(!name)
+			return false;
+		else if(name === 'all')
+			return this.allSelected;
+
+		else {
+			return (this.selected.indexOf(name)>-1); //do we need parentheses? 
+		}
+	};
+})
