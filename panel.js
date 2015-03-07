d3ae7a304bdbec9e0d20ead58b95ab68fa5d06e7
Rachel <rachel.nicole.tucker@gmail.com>
2015-03-07 15:07:54 -0600

    made side bar dependent on cookies

--- panel.js
+++ panel.js
@@ -1,45 +1,65 @@
-var app = angular.module('panelModule', ['ui.bootstrap']);
+var app = angular.module('panelModule', ['ui.bootstrap', 'ngCookies']);
 
-app.controller('PanelController', function(){
-	this.allSelected = false;
-	//look for cookie
-	this.players = [
-		'TheOddOne','Imaqtpie','Bjergsen' //we need to read this from a json file
-	];	
-	
-	this.selected = [];
-	this.select = function(name){
-		if(!name || (name === 'all' && !this.allSelected)){
-			this.selected=[]; //this is to prevent cases where a player is selected then added into the list again when the 'all' button is pushed. I don't know if this would cause any bugs, but I'm just doing this to be safe.
-			for(p in this.players){
-				this.selected.push(this.players[p]); //select all players if no parameter is passed or if 'all' is clicked
-			}
+app.controller('PanelController', ['$cookies', function($cookies){
+    //look for cookie
+    //NOTE: this behavior is deprecated. In newer angular, cookies use .get and .put functions, but we are using
+    //1.3.14, which doesn't have this yet.
+    var playersCookie = $cookies.oneGuide_players;
 
-			this.allSelected = true;
-		}
+    if(playersCookie == undefined){
+	var playersCookieObj = {"selected": [], "allSelected": false, "players": ["TheOddOne", "Imaqtipie", "Bjergsen"]};
+	var jsonplayers = JSON.stringify(playersCookieObj);
+	$cookies.oneGuide_players = JSON.stringify(playersCookieObj);
+    }
+    playersCookie = JSON.parse($cookies.oneGuide_players);
 
-		else if(name === 'all' && this.allSelected){
-			this.selected = [];
-			this.allSelected = false;
-		}
+    this.players = playersCookie.players;	
+    this.allSelected = playersCookie.allSelected;
+    this.selected = playersCookie.selected;
+    this.select = function(name){
+	if(!name || (name === 'all' && !this.allSelected)){
+	    this.selected=[]; //this is to prevent cases where a player is selected then added into the list again when the 'all' button is pushed. I don't know if this would cause any bugs, but I'm just doing this to be safe.
+	    
+	    for(p in this.players){
+		this.selected.push(this.players[p]); //select all players if no parameter is passed or if 'all' is clicked
+	    }
 
-		else { //name isn't 'all'
-			this.allSelected = false;	
-			if(this.selected.indexOf(name)>-1){
-				this.selected.splice(this.selected.indexOf(name), 1); //remove at index of name
-			}
-			else this.selected.push(name);
+	    this.allSelected = true;
+	    playersCookie.allSelected = true;
+	    playersCookie.selected = this.selected;
+	    $cookies.oneGuide_players = JSON.stringify(playersCookie);
+	}
 
-		}
-	};
-	this.isSelected = function(name){
-		if(!name)
-			return false;
-		else if(name === 'all')
-			return this.allSelected;
+	else if(name === 'all' && this.allSelected){
+	    this.selected = [];
+	    this.allSelected = false;
+	    playersCookie.allSelected = false;
+	    playersCookie.selected = [];
+	    $cookies.oneGuide_players = JSON.stringify(playersCookie);
+	}
 
-		else {
-			return (this.selected.indexOf(name)>-1); //do we need parentheses? 
-		}
-	};
-});
+	else { //name isn't 'all'
+	    this.allSelected = false;
+	    playersCookie.allSelected = false;
+	    if(this.selected.indexOf(name)>-1){
+		this.selected.splice(this.selected.indexOf(name), 1); //remove at index of name
+		playersCookie.selected = this.selected;
+	    }
+	    else{
+		this.selected.push(name);
+		playersCookie.selected = this.selected;
+	    }
+	    $cookies.oneGuide_players = JSON.stringify(playersCookie);
+	}
+    };
+    this.isSelected = function(name){
+	if(!name)
+	    return false;
+	else if(name === 'all')
+	    return this.allSelected;
+
+	else {
+	    return (this.selected.indexOf(name)>-1); //do we need parentheses? 
+	}
+    };
+}]);
