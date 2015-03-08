0a3f8233f0fd8141b35b4d361b3d5b8c5898e0a1
Rachel <rachel.nicole.tucker@gmail.com>
2015-03-08 17:58:00 -0500

    editing functionality for sidebar

--- panel.js
+++ panel.js
@@ -1,13 +1,13 @@
 var app = angular.module('panelModule', ['ui.bootstrap', 'ngCookies']);
 
-app.controller('PanelController', ['$cookies', function($cookies){
+app.controller('PanelController', ['$cookies', '$modal', function($cookies, $modal){
     //look for cookie
     //NOTE: this behavior is deprecated. In newer angular, cookies use .get and .put functions, but we are using
     //1.3.14, which doesn't have this yet.
     var playersCookie = $cookies.oneGuide_players;
-
+    
     if(playersCookie == undefined){
-	var playersCookieObj = {"selected": [], "allSelected": false, "players": ["TheOddOne", "Imaqtipie", "Bjergsen"]};
+	var playersCookieObj = {"selected": [], "allSelected": false, "players": []};
 	var jsonplayers = JSON.stringify(playersCookieObj);
 	$cookies.oneGuide_players = JSON.stringify(playersCookieObj);
     }
@@ -62,4 +62,87 @@ app.controller('PanelController', ['$cookies', function($cookies){
 	    return (this.selected.indexOf(name)>-1); //do we need parentheses? 
 	}
     };
+
+    //the following sets up the add player modal
+    panel = this;
+   this.openModal = function() {
+       var addPlayerModal = $modal.open({
+	   templateUrl: 'addPlayerModal.html',
+	   controller: 'AddPlayerController as addPlayer',
+	   size: 'lg',
+	   //here we pass the modal the current players
+	   resolve: {
+	       players: function() {
+		   //slice because we want a copy
+		   return panel.players.slice();
+	       }
+	   }
+       });
+
+       //this is triggered when this modal goes away
+       addPlayerModal.result.then(function (newPlayers) {
+	   //on 'save' we update the new list of players
+	   panel.players = newPlayers;
+	   playersCookie.players = panel.players;
+	   $cookies.oneGuide_players = JSON.stringify(playersCookie);
+       }, function (){
+	   //on dismiss we do nothing
+       });
+   };
 }]);
+
+//this is what actually controls the modal
+//we get 'players' because we passed it using the resolve key in the above $modal.open function
+angular.module('panelModule').controller('AddPlayerController', function($http, $modalInstance, players) {
+    modalController = this;
+    modalController.players = players;
+    modalController.summonerName = undefined;
+    modalController.alertClasses = 
+	{'alert-danger': false,
+	 'alert': true};
+    modalController.alertMsg = "Player added!";
+    modalController.submitted = false;
+
+    modalController.attemptAdd = function () {
+	var currPlayers = modalController.players;
+	var inputedName = modalController.summonerName;
+	console.log(currPlayers);
+	var index = currPlayers.indexOf(inputedName);
+	//already in list?
+	if(index > -1){
+	    modalController.alertMsg = "You are already tracking summoner " + inputedName;
+	    modalController.alertClasses['alert-danger'] = true;
+	    modalController.submitted = true;
+	}
+	else{    
+	    $http.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + modalController.summonerName
+		      + "?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff").
+		success(function(data){
+		    modalController.players.push(modalController.summonerName);
+		    modalController.submitted = false;
+		    modalController.summonerName = undefined;
+		}).
+		//summoner not found
+		error(function(data){
+		    modalController.alertMsg = "We couldn't find the summoner " + modalController.summonerName;
+		    modalController.alertClasses['alert-success'] = false;
+		    modalController.alertClasses['alert-danger'] = true;
+		    modalController.submitted = true;
+		});
+	}
+    };
+	    
+    modalController.remove = function(player) {
+	var playerIndex = modalController.players.indexOf(player);
+	console.log(playerIndex);
+	modalController.players.splice(playerIndex, 1);
+    };
+    
+    modalController.save = function () {
+	$modalInstance.close(modalController.players);
+    };
+    
+    modalController.cancel = function () {
+	$modalInstance.dismiss('cancel');
+    };
+});
