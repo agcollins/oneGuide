40448ccfa19e44dd8faf6ce6909da15e41792c9b
agcollins <agcollins@utexas.edu>
2015-03-10 02:12:00 -0500

    proBuild?

--- panel.js
+++ panel.js
@@ -22,53 +22,49 @@ app.controller('PanelController', ['$cookies', '$modal', '$scope', '$http', func
 	$scope.selected = this.selected;
 
 	var controller = this;
-	var proBuild = [];
+	controller.proBuild = [];
 
 	function getQueryStringValue (key) {  
 		return unescape(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));  
 	}  
 
-	function getPlayerId(name){
+	function getPlayerId(name, after){
 		if(name=='undefined')
 			console.log('poop');
 		$http.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'+name+'?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff').success(function(data){
 			//these keys are all lower case
-			return data[name.toLowerCase()].id;
-		}); 
-		//https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Oshinova?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff
+			after(data[name.toLowerCase()].id, name);		
+	}); 
+	//https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Oshinova?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff
 	}
 
 	controller.selectedID = getQueryStringValue("champion");
 
-	$http.get("https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff").success(function(data){
-		var champs = data["data"]
-		for (var key in champs){
-			if(champs[key].id == controller.selectedID){
-				controller.selectedChampion = champs[key];
-				controller.selectedChampion["squareimg"] = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/" + key + ".png";
-				controller.selectedChampion["loadingimg"] = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + key + "_0.jpg";
-			}
-		}
-	});
-
 	for(var p in controller.players){
-		var id = getPlayerId(controller.players[p]);	
-		console.log(id);
-		$http.get("http://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/" + getPlayerId(controller.players[p]) + "/recent?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff").success(function(data) {
-			var games = data.games;
-			for(var i = 0; i < games.length; i++){
-				game = games[i];
-				if(game.championId == controller.selectedID){
-					var stats = game.stats;
-					for(var j = 1; j <= 6; j++){
-						proBuild.push({"id": stats["item" + j], 
-							"imgsrc": "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/" 
-							+ stats["item" + j] + ".png"});
+		function help(id, name){
+			$http.get("https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/" + id + "/recent?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff").success(function(data) {
+
+				console.log(data);
+				//this is the past 10 games from the summoner.
+				console.log(name);
+				console.log(proBuild);
+				var games = data.games;
+				for(var i = 0; i < games.length; i++){
+					game = games[i];
+					if(game.championId == controller.selectedID){
+						var stats = game.stats;
+						for(var j = 1; j <= 6; j++){
+							controller.proBuild[name].push({"id": stats["item" + j], 
+								"imgsrc": "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/" 
+								+ stats["item" + j] + ".png"});
+						}
+						console.log(proBuild);
+						break;
 					}
-					break;
 				}
-			}
-		})
+			})
+		}
+		getPlayerId(controller.players[p], help);
 	};
 
 	this.select = function(name){
