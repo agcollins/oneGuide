110c191f65f6fe1972595e9d0ffb6b710c5c5d7a
Alex <agcollins@utexas.edu>
2015-03-16 15:14:37 -0500

    cleaned up the build.js code which still is only temporary

--- build.js
+++ build.js
@@ -8,23 +8,23 @@ app.controller("BuildController", ['$http', function ($http) {
     controller.proBuild = [];
     controller.builds = {};
 
-    function getQueryStringValue (key){
-        return unescape(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));  
+    function getQueryStringValue(key) {
+        return unescape(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
     }
 
     controller.selectedID = getQueryStringValue("champion");
 
-    $http.get("https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff").success(function(data){
-        var champs = data["data"]
-        for (var key in champs){
-            if(champs[key].id == controller.selectedID){
+    $http.get("https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=43e187ef-e56e-4f24-bd58-1dbdc841abff").success(function (data) {
+        var key, champs = data.data;
+        for (key in champs) {
+            if (champs[key].id == controller.selectedID) {
                 controller.selectedChampion = champs[key];
-                controller.selectedChampion["squareimg"] = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/" + key + ".png";
-                controller.selectedChampion["loadingimg"] = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + key + "_0.jpg";
+                controller.selectedChampion.squareimg = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/" + key + ".png";
+                controller.selectedChampion.loadingimg = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + key + "_0.jpg";
             }
             controller.champions.push(champs[key]);
-            controller.champNames.push(champs[key]["name"]);
+            controller.champNames.push(champs[key].name);
         }
     });
-    
+
 }]);
